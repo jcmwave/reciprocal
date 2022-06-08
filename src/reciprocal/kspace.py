fixed bug that weighting was incorrectly ordered compared to point sampling

src/reciprocal/kspace.py
@@ -343,6 +343,7 @@ class RegularSampler(Sampler):
                     pass
                     #continue
                 wedge_area = 0.5*(upper_radius**2-lower_radius**2)*np.radians(phi_spacing)
+                #print(upper_radius, lower_radius, phi_spacing, wedge_area/total_area)
                 weighting.append(wedge_area/total_area)
                 all_points.append(trial_point)
                 wedge = Wedge(np.array([0., 0.]), upper_radius,
@@ -351,9 +352,11 @@ class RegularSampler(Sampler):
                 artists.append(wedge)
 
         all_point_array = np.vstack(all_points)
-        all_point_array = order_lexicographically(all_point_array)
+        all_point_array, sort_indices = order_lexicographically(all_point_array,
+                                                       return_sort_indices=True)
         all_kvs = self.kspace.convert_to_KVectors(all_point_array, 1., 1.)
         weighting_array = np.array(weighting)
+        weighting_array = weighting_array[sort_indices]
         if return_artists:
             return all_kvs, weighting_array, artists
         else:
@@ -402,9 +405,11 @@ class RegularSampler(Sampler):
                 all_points.append(trial_point)
 
         all_point_array = np.vstack(all_points)
-        all_point_array = order_lexicographically(all_point_array)
+        all_point_array, sort_indices = order_lexicographically(all_point_array,
+                                                       return_sort_indices=True)
         all_kvs = self.kspace.convert_to_KVectors(all_point_array, 1., 1.)
         weighting_array = np.array(weighting)
+        weighting_array = weighting_array[sort_indices]
         if return_artists:
             return all_kvs, weighting_array, artists
         else:

src/reciprocal/utils.py
@@ -159,7 +159,7 @@ def lies_on_vertex(point, named_vertices, rel_tol=1e-3):
             return (True, special_point)
     return (False, '')
 
-def order_lexicographically(points, start=0.0):
+def order_lexicographically(points, start=0.0, return_sort_indices=False):
     angle = np.angle( (points[:,0]+1j*points[:,1])*np.exp(1j*(np.pi+1e-3+start)))
     angle = np.round(angle, 3)
     radius = np.linalg.norm(points, axis=1)
@@ -172,7 +172,10 @@ def order_lexicographically(points, start=0.0):
     #pp.pprint(all_data)
     #pp.pprint("x, y, z, angle, radius")
     #pp.pprint(all_data[sort_indices, :])
-    return points[sort_indices, :]
+    if return_sort_indices:
+        return points[sort_indices, :], sort_indices
+    else:
+        return points[sort_indices, :]
 
 class BravaisLattice(Enum):
     HEXAGON = 0