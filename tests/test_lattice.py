c9118c36fe1970453f8d010251aa7a2973a8850a
gjjring <manley@zib.de>
2023-01-05 11:30:43 +0100

    updated tests for new syntax

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -2,14 +2,9 @@ import pytest
 from reciprocal import lattice
 import numpy as np
 
-@pytest.fixture
-def fail_state_msg():
-    fail_message = ("inputs must either be two vector lengths and "+
-                    "lattice angle or the lattice vectors")
-    yield fail_message
 
 def test_lattice_vectors_lengths():
-    lat = lattice.LatticeVectors(1000, 1000, 90.)
+    lat = lattice.LatticeVectors.from_lengths_angle(1000, 1000, 90.)
     tol = 1e-6
     assert abs(lat.vec1[0]-1000.) < tol
     assert abs(lat.vec1[1]-0.) < tol
@@ -19,8 +14,7 @@ def test_lattice_vectors_lengths():
 def test_lattice_vectors_vectors():
     vec1 = np.array([500., 250.])
     vec2 = np.array([500., -250.])
-    lat = lattice.LatticeVectors(vector1=vec1,
-                                 vector2=vec2)
+    lat = lattice.LatticeVectors(vec1, vec2)
     tol = 1e-6
     assert abs(lat.vec1[0]-500.) < tol
     assert abs(lat.vec1[1]-250.) < tol
@@ -31,7 +25,7 @@ def test_lattice_vectors_angle():
     a = 1000.
     b = 1000.
     angle = 45.
-    lat = lattice.LatticeVectors(a, b, angle)
+    lat = lattice.LatticeVectors.from_lengths_angle(a, b, angle)
     tol = 1e-6
     v1 = a*np.array([1., 0.])
     v2 = b*np.array([np.cos(np.radians(angle)),
@@ -41,34 +35,12 @@ def test_lattice_vectors_angle():
     assert abs(lat.vec2[0]-v2[0]) < tol
     assert abs(lat.vec2[1]-v2[1]) < tol
 
-def test_lattice_vectors_failstate1(fail_state_msg):
-    try:
-        lat = lattice.LatticeVectors(1000, 1000)
-    except ValueError as e:
-        if str(e) != fail_state_msg:
-            raise e
-
-def test_lattice_vectors_failstate2(fail_state_msg):
-    try:
-        lat = lattice.LatticeVectors(length1=1000, angle=45)
-    except ValueError as e:
-        if str(e) != fail_state_msg:
-            raise e
-
-def test_lattice_vectors_failstate3(fail_state_msg):
-    try:
-        lat = lattice.LatticeVectors(vector1=[100., 0.],
-                                     vector2=[0., 100.],
-                                     angle=45)
-    except ValueError as e:
-        if str(e) != fail_state_msg:
-            raise e
 
 def test_reciprocal_vectors():
     a = 1000.
     b = 1000.
     angle = 60.
-    lat = lattice.LatticeVectors(a, b, angle)
+    lat = lattice.LatticeVectors.from_lengths_angle(a, b, angle)
     rlat = lat.reciprocal_vectors()
     tol = 1e-6
     assert abs(rlat.vec1[0]-0.00628319) < tol
@@ -77,4 +49,15 @@ def test_reciprocal_vectors():
     assert abs(rlat.vec2[1]-0.0072552) < tol
 
 def test_lattice():
-    lat = lattice.Lattice(1000., 1000., 60.)
+    a = 1000.
+    b = 1000.
+    angle = 60.
+    lat_vectors = lattice.LatticeVectors.from_lengths_angle(a, b, angle)
+    lat = lattice.Lattice(lat_vectors)
+
+def test_lattice_from_keywords():
+    lat_vec_args = {}
+    lat_vec_args['length1'] = 1000.
+    lat_vec_args['length2'] = 1000.
+    lat_vec_args['angle'] = 60.    
+    lat = lattice.Lattice.from_lat_vec_args(**lat_vec_args)
